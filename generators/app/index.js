07a704f52c687e7c12c5bc194464eae79255b136
roibh <roi@ewave.co.il>
2016-08-06 02:00:54 +0300

    the basics

---
+++ generators/app/index.js
@@ -0,0 +1,41 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.Base.extend({
+  prompting: function () {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the best ' + chalk.red('generator-nodulus') + ' generator!'
+    ));
+
+    var prompts = [{
+      type: 'confirm',
+      name: 'someAnswer',
+      message: 'Would you like to enable this option?',
+      default: true
+    }];
+
+    return this.prompt(prompts).then(function (props) {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    }.bind(this));
+  },
+
+  writing: function () {
+    this.fs.copy(
+      this.templatePath('./nodule_files'),
+      this.destinationPath('./')
+    );
+
+
+   
+
+
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
